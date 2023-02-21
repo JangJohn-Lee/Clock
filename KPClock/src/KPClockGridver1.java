d1669ca9f4c901b789dbb75c186638304262a507
John <sangrock510@naver.com>
2023-02-21 17:06:56 +0900

    .

--- KPClock/src/KPClockGridver1.java
+++ KPClock/src/KPClockGridver1.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class KPClockGridver1 {
 
 	public static void main(String[] args) {
-		
+		//수정
 		Scanner sc = new Scanner(System.in);
 		System.out.print("AM/PM or 24hours?(24hours = 1 , AM/PM = 2)");
 		String choice = sc.nextLine();
